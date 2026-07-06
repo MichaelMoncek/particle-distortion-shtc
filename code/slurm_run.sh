a8a4c6869017da930c266730ffc56a9d0511b85d
Michael Moncek <michael.moncek@seznam.cz>
2026-07-06 02:31:07 +0200

    slurm script

--- code/slurm_run.sh
+++ code/slurm_run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+#SBATCH --partition=math
 #SBATCH --array=0-1
-julia -t auto slurm_run_task.jl
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --exclusive
+#SBATCH --job-name=plate
+#SBATCH --output=slurm-%A_%a.out
+export JULIA_NUM_THREADS=${SLURM_CPUS_ON_NODE}
+
+ch-run --bind=$HOME:/mnt/0 julia-1.12.5 -- \
+    /usr/local/julia/bin/julia -t ${JULIA_NUM_THREADS} slurm_run_task.jl
